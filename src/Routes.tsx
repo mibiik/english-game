ed28648ff3fb49cb53076941b0d626bfdeb2dc1d
User <user@example.com>
2025-05-07 10:52:35 +0300

    Güncelleme mesajı

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -8,6 +8,8 @@ import { MultipleChoice } from './components/GameModes/MultipleChoice';
 import { FlashCard } from './components/GameModes/FlashCard';
 import { SpeakingGame } from './components/GameModes/SpeakingGame';
 import { WordRace } from './components/GameModes/WordRace';
+import { MemoryGame } from './components/GameModes/MemoryGame';
+import { TimedMatchingGame } from './components/GameModes/TimedMatchingGame';
 import { words } from './data/words';
 import { Navbar } from './components/Navbar';
 
@@ -32,6 +34,8 @@ export const AppRoutes: React.FC = () => {
           <Route path="/flashcard" element={<FlashCard words={words} unit={currentUnit} />} />
           <Route path="/speaking" element={<SpeakingGame words={words} unit={currentUnit} />} />
           <Route path="/word-race" element={<WordRace words={words} unit={currentUnit} />} />
+          <Route path="/memory-game" element={<MemoryGame words={words} unit={currentUnit} />} />
+          <Route path="/timed-matching-game" element={<TimedMatchingGame words={words} unit={currentUnit} />} />
         </Routes>
       </div>
     </>
